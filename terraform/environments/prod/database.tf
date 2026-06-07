0e17f43612f008596340ea7c27de6f4d1bdebc2b
andres31416YT <andrespaganroncall@gmail.com>
2026-06-07 09:40:05 -0500

    fix(terraform): extract outputs.tf and align dev/prod metadata

--- terraform/environments/prod/database.tf
+++ terraform/environments/prod/database.tf
@@ -190,19 +190,4 @@ resource "aws_iam_role_policy_attachment" "rds_monitoring" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
 }
 
-output "db_endpoint" {
-  value = aws_db_instance.main.address
-}
 
-output "db_name" {
-  value = aws_db_instance.main.db_name
-}
-
-output "redis_endpoint" {
-  value = aws_elasticache_replication_group.main.primary_endpoint_address
-}
-
-output "redis_auth_token" {
-  value     = random_string.redis_password.result
-  sensitive = true
-}
