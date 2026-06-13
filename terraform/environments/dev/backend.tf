9324260dc1f39c258246f63e20bb2706c5541e8d
andres31416YT <andrespaganroncall@gmail.com>
2026-06-13 18:29:50 -0500

    refactor: remove dynamodb_table configuration from backend.tf files

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "truetally-terraform-state"
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "truetally-terraform-locks"
+    
     encrypt        = true
   }
 }
--- terraform/environments/prod/backend.tf
+++ terraform/environments/prod/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "truetally-terraform-state"
     key            = "prod/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "truetally-terraform-locks"
+    
     encrypt        = true
   }
 }
