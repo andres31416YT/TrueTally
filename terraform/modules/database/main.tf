bd62ebf90c9083e32760e1c180daf4a186e65b09
andres31416YT <andrespaganroncall@gmail.com>
2026-06-13 18:16:13 -0500

    feat: add outputs for Redis endpoint and port in database module

--- terraform/modules/database/main.tf
+++ terraform/modules/database/main.tf
@@ -183,3 +183,11 @@ output "elasticache_address" {
 output "elasticache_port" {
   value = aws_elasticache_replication_group.main.port
 }
+
+output "redis_endpoint" {
+  value = aws_elasticache_replication_group.main.primary_endpoint_address
+}
+
+output "redis_port" {
+  value = aws_elasticache_replication_group.main.port
+}
