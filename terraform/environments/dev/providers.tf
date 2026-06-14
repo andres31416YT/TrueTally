1bd89444774c362aa4d7e24e1a52fbce535f02c4
andres31416YT <andrespaganroncall@gmail.com>
2026-06-13 19:08:49 -0500

    fix: database password as output variables in Terraform workflow and create provider configurations for dev and prod environments

---
+++ terraform/environments/dev/providers.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Project     = var.project_name
+      Environment = var.environment
+      ManagedBy   = "terraform"
+    }
+  }
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
