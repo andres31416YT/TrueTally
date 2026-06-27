c4ec74d1b301362a42b280bece23e3e71eedc65a
marlon <jchavezv3@upao.edu.pe>
2026-06-27 11:32:56 -0500

    fix(cloudwatch [CKV_AWS_338]): update retention period for log groups to 365 days

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "main" {
 
 resource "aws_cloudwatch_log_group" "vpc_flow_log" {
   name              = "${local.name_prefix}-vpc-flow-logs"
-  retention_in_days = 7
+  retention_in_days = 365
   kms_key_id        = var.kms_key_arn
 }
 
