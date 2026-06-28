8bc0c0a2ac10a6c5a51464ccf27891ea6c6aa059
andres31416YT <andrespaganroncall@gmail.com>
2026-06-28 18:38:18 -0500

    refactor(terraform): remove redundant ECR DKR VPC endpoint resource

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -253,13 +253,3 @@ resource "aws_vpc_endpoint" "ecr_dkr" {
   tags = { Name = "${local.name_prefix}-ecr-dkr-endpoint" }
 }
 
-resource "aws_vpc_endpoint" "ecr_dkr" {
-  vpc_id              = aws_vpc.main.id
-  service_name        = "com.amazonaws.${var.aws_region}.ecr.dkr"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = [for s in aws_subnet.blockchain : s.id]
-  security_group_ids  = [aws_security_group.lambda.id]
-  private_dns_enabled = false
-
-  tags = { Name = "${local.name_prefix}-ecr-dkr-endpoint" }
-}
